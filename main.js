c566f3b5fa335105aa2cfac7987c6225be51160b
Nour Bouzaffara <nbouzaffara@manh.com>
2019-03-25 13:59:48 +0100

    done

---
+++ main.js
@@ -0,0 +1,18 @@
+const greetings = require("./greeting.js");
+const mingling = require("./mingling.js");
+
+// "Hello"
+greetings.greetingEnglish();
+
+// "Konichiwa"
+greetings.greetingJapanese();
+
+// "Hola"
+greetings.greetingSpanish();
+
+// "Halló"
+greetings.greetingIcelandic();
+
+mingling.minglingFrench();
+
+mingling.minglingSpanish();
---
+++ mingling.js
@@ -0,0 +1,7 @@
+exports.minglingSpanish = function(){
+ console.log("Me llamo Nour, y tu?");
+}
+
+exports.minglingFrench = function(){
+ console.log("Je m'apelle Nour et toi?");
+}
